0a96d7cdb9048969df54bab4050f8a2861822278
Mikhail Vodichev <gierr7976@gmail.com>
2021-08-13 12:15:58 +0300

    Добавлена константа для средней длительности анимации
    Добавлена константа MEDUIM_ANIMATION_DURATION, определяющая среднюю длительность анимации

--- ui/lib/src/misc/constants.dart
+++ ui/lib/src/misc/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/widgets.dart';
 
-const SMALL_ANIMATION_DURATION = Duration(milliseconds: 300);
+const SMALL_ANIMATION_DURATION = Duration(milliseconds: 400);
+
+const MEDIUM_ANIMATION_DURATION = Duration(milliseconds: 600);
 
 const GENERIC_BORDER_RADIUS = BorderRadius.all(Radius.circular(16));
