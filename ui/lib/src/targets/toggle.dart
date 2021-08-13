41ee5765af90650909e227b8e77f62059b234841
Mikhail Vodichev <gierr7976@gmail.com>
2021-08-13 11:45:15 +0300

    Рефакторинг MementoToggle

--- ui/lib/src/targets/toggle.dart
+++ ui/lib/src/targets/toggle.dart
@@ -25,74 +25,62 @@ class MementoToggle extends Toggleable {
   @override
   Widget builder(BuildContext context, bool state) => _ground(context, state);
 
-  Clickable _ground(BuildContext context, bool state) {
-    return Clickable(
-      child: _content(context, state),
-      enabled: enabled,
-      color: state && enabled ? MementoColorTheme.of(context).dimmedOk : null,
-      onTap: () => tap(state, context),
-    );
-  }
+  Clickable _ground(BuildContext context, bool state) => Clickable(
+        child: _content(context, state),
+        enabled: enabled,
+        color: state && enabled ? MementoColorTheme.of(context).dimmedOk : null,
+        onTap: () => tap(state, context),
+      );
 
-  Padding _content(BuildContext context, bool state) {
-    return Padding(
-      padding: EdgeInsets.only(left: 16, top: 8, right: 16, bottom: 8),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            child: description != null
-                ? _withDescription(context, state)
-                : _title(context, state),
-          ),
-          _icon(state, context),
-        ],
-      ),
-    );
-  }
-
-  Column _withDescription(BuildContext context, bool state) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        _title(context, state),
-        _description(state, context),
-      ],
-    );
-  }
-
-  Padding _icon(bool state, BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: 16),
-      child: Icon(
-        state ? TablerIcons.check : TablerIcons.x,
-        size: 24,
-        color: _titleColor(state, context),
-      ),
-    );
-  }
-
-  Padding _description(bool state, BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: 8, bottom: 8),
-      child: Text(
-        description!,
-        style: MementoText.small.copyWith(
-          color: _descriptionColor(state, context),
+  Padding _content(BuildContext context, bool state) => Padding(
+        padding: EdgeInsets.only(left: 16, top: 8, right: 16, bottom: 8),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: description != null
+                  ? _withDescription(context, state)
+                  : _title(context, state),
+            ),
+            _icon(state, context),
+          ],
         ),
-        textAlign: TextAlign.justify,
-        softWrap: true,
-      ),
-    );
-  }
+      );
 
-  Color _descriptionColor(bool state, BuildContext context) {
-    return enabled
-        ? state
-            ? MementoColorTheme.of(context).ok
-            : MementoColorTheme.of(context).almostDimmedText
-        : MementoColorTheme.of(context).dimmedText;
-  }
+  Column _withDescription(BuildContext context, bool state) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _title(context, state),
+          _description(state, context),
+        ],
+      );
+
+  Padding _icon(bool state, BuildContext context) => Padding(
+        padding: EdgeInsets.only(left: 16),
+        child: Icon(
+          state ? TablerIcons.check : TablerIcons.x,
+          size: 24,
+          color: _titleColor(state, context),
+        ),
+      );
+
+  Padding _description(bool state, BuildContext context) => Padding(
+        padding: EdgeInsets.only(top: 8, bottom: 8),
+        child: Text(
+          description!,
+          style: MementoText.small.copyWith(
+            color: _descriptionColor(state, context),
+          ),
+          textAlign: TextAlign.justify,
+          softWrap: true,
+        ),
+      );
+
+  Color _descriptionColor(bool state, BuildContext context) => enabled
+      ? state
+          ? MementoColorTheme.of(context).ok
+          : MementoColorTheme.of(context).almostDimmedText
+      : MementoColorTheme.of(context).dimmedText;
 
   Widget _title(BuildContext context, bool state) => Text(
         title,
@@ -102,11 +90,9 @@ class MementoToggle extends Toggleable {
         softWrap: true,
       );
 
-  Color _titleColor(bool state, BuildContext context) {
-    return enabled
-        ? state
-            ? MementoColorTheme.of(context).strongOk
-            : MementoColorTheme.of(context).semiDimmedText
-        : MementoColorTheme.of(context).dimmedText;
-  }
+  Color _titleColor(bool state, BuildContext context) => enabled
+      ? state
+          ? MementoColorTheme.of(context).strongOk
+          : MementoColorTheme.of(context).semiDimmedText
+      : MementoColorTheme.of(context).dimmedText;
 }
