063cade822555991785aaa5d1cd257b6ad2d88aa
Mikhail Vodichev <gierr7976@gmail.com>
2021-08-19 16:53:48 +0300

    Collapsible (UI)
    - Реализован виджет MementoCollapsible

---
+++ ui/example/layout/collapsible.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:memento_style/memento_style.dart';
+import 'package:memento_ui/src/data/listing/listing.dart';
+import 'package:memento_ui/src/layout/collapsible/collapsible.dart';
+
+import '../preset.dart';
+
+void main() {
+  TestPreset(child: CollapsibleTest()).run();
+}
+
+class Data {
+  final String header;
+  final String body;
+
+  Data({
+    required this.header,
+    required this.body,
+  });
+}
+
+class CollapsibleTest extends StatelessWidget with MementoColorThemeUserMixin {
+  const CollapsibleTest({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MementoListing<Data>(
+        elements: [
+          Data(
+              header: 'Группа 1',
+              body:
+                  'Я опять сегодня прыгаю на кровати потому что у меня всё в лимонаде'),
+          Data(
+              header: 'Группа 2',
+              body:
+                  'Я опять сегодня прыгаю на кровати потому что у меня всё в лимонаде'),
+        ],
+        builder: (context, index, element) => MementoCollapsible(
+          headerBuilder: (context) => Text(
+            element.header,
+            style: MementoText.mediumLabel
+                .copyWith(color: colorTheme(context).semiDimmedText),
+          ),
+          bodyBuilder: (context) => Text(
+            element.body,
+            style: MementoText.small
+                .copyWith(color: colorTheme(context).almostDimmedText),
+            softWrap: true,
+          ),
+        ),
+      );
+}
