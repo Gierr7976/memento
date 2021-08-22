cd4989b5c0c48f931c62fd3f78e29263d7246a4c
Mikhail Vodichev <gierr7976@gmail.com>
2021-08-22 17:02:29 +0300

    Валидация InputState (UI)
    - Добавлена проверка, запрещающая создавать InputState,
       несущий недопустимое состояние

--- ui/lib/src/logic/input.dart
+++ ui/lib/src/logic/input.dart
@@ -11,7 +11,7 @@ class InputState {
     this.error = false,
     this.valid = false,
     this.data = '',
-  });
+  }) : assert(focused && !error && !valid || !focused);
 
   InputState copyWith({
     String? data,
